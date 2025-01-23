13ddcb7f8f282fa9248c3238c5c1f6c6f2633858
Dory509 <dorycelestin@outlook.com>
2025-01-23 15:10:50 -0500

    page html start

---
+++ routes/html.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const app = require("express").Router()
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  );
+  
+  // GET Route for feedback page
+  app.get('/feedback', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+  );
+  // Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  );
+
+
+  module.exports = app
